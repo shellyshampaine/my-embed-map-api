22138af4e2b29af0463d441075467c8e3e40fbe2
shellyshampaine <46080705+shellyshampaine@users.noreply.github.com>
2020-04-09 20:48:33 +0600

    Update map.py

--- map.py
+++ map.py
@@ -9,4 +9,4 @@ def hello_world():
     req = '<iframe width="600" height="800" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/directions?origin=' + coords1 + '&destination=' + coords2 + '&mode=walking&key=AIzaSyDwCpjPILFW4SHton4y3ThUU9KNip1xOJM" allowfullscreen></iframe>'
     return req
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
